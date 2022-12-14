a401ae0dfa8821b74ef32da3301124d697941dc1
hanbyian325 <ian@goserendip.com>
2022-12-14 17:37:20 -0500

    changed slice data types

--- src/components/StudentTest.js
+++ src/components/StudentTest.js
@@ -1,30 +1,30 @@
 import React, { useState }from "react";
-import { useSelector, useDispatch} from 'react-redux';
+import { useSelector} from 'react-redux';
 import styles from './StudentTestStyle'
 import { Link } from "react-router-dom";
-import speaker from '../assets/speakerToo.png'
+import speaker from '../assets/speaker.png'
 //import { addMyWords, removeMyWords, addMasteredWords, removeMasteredWords } from '../redux/slices/words';
 
 const Test = (()=>{
-    const dispatch = useDispatch();//dispatch allows you to use reducers written in a slice
-    const myWords = useSelector(state => state.word.myWords);//useSelector() allows access to state variables
-    const [currentSpelling, changeSpelling] = useState("");
-    const [testQuestion, incrementQuestion] = useState(0);
-    const [testResults, addResult] = useState([]);
-    const hearWord = () => {
-        //get audio file for the word the user is currently on and play it.
+    const [spellingInput, setSpellingInput] = useState("");
+    const [questionIndex, setQuestionIndex] = useState(0);
+    const [studentSpellings, setStudentSpellings] = useState({});
+    const myWords = useSelector(state => state.word.myWords);
 
-        const wordAudio = require("../assets/"+ myWords[testQuestion]+".mp3");
+    const hearWord = () => {
+        const wordAudio = require("../assets"+ myWords[questionIndex].audio);
         new Audio(wordAudio).play();
     }
     const handleNextQuestion = () => {
-        incrementQuestion(testQuestion+1);
-        addResult([...testResults,currentSpelling]);
-        changeSpelling("");
-        
+        let tempSpellings = {...studentSpellings};
+        tempSpellings[myWords[questionIndex].word]=spellingInput;
+        setStudentSpellings(tempSpellings);
+        console.log(studentSpellings);
+        setQuestionIndex(questionIndex+1);
+        setSpellingInput("");
     }
     return (
-        testQuestion<myWords.length?
+        questionIndex<myWords.length?
         <div style={styles.testContainer}>
             <h1 style={styles.header}>Welcome to your Spelling Safari test</h1>
             <div style={styles.questionContainer}>
@@ -33,9 +33,10 @@ const Test = (()=>{
                         <img src={speaker} alt="speaker" style={styles.image}></img>
                         Play Audio
                     </button>
+                    <p><strong>Question {questionIndex+1}</strong></p>
                 </div>
                 <div style={{display:'flex',justifyContent:'center'}}>
-                    <input type="text" placeholder="spell the word you hear" value={currentSpelling} onChange={(e)=>{changeSpelling(e.target.value)}} style={styles.spellingInput}></input>
+                    <input type="text" placeholder="spell the word you hear" value={spellingInput} onChange={(e)=>{setSpellingInput(e.target.value)}} style={styles.spellingInput}></input>
                     <button onClick={handleNextQuestion} style={styles.submitButton}>Submit Answer</button>
                 </div>
             </div>
--- src/redux/slices/words.js
+++ src/redux/slices/words.js
@@ -1,54 +1,88 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-// future feature? myWords = {id:int, word:"", audio:path to audio file from assets folder}
-/*
-  {id:0, word:"bird", audio:"/assets/bird.mp3"},
-  {id:1, word:"baseball", audio:"/assets/baseball.mp3"},
-  {id:2, word:"ball", audio:"/assets/ball.mp3"},
-  {id:3, word:"therefore", audio:"/assets/therefore.mp3"},
-  {id:4, word:"rook", audio:"/assets/rook.mp3"},
-  {id:5, word:"arithmetic", audio:"/assets/arithmetic.mp3"},
-  {id:6, word:"paint", audio:"/assets/paint.mp3"},
-  {id:7, word:"library", audio:"/assets/library.mp3"},
-  {id:8, word:"ketchup", audio:"/assets/ketchup.mp3"},
-  {id:9, word:"candle", audio:"/assets/candle.mp3"}
-  
-*/
 const initialState = {
-  words:[],
-  myWords:["bird","baseball","ball","therefore","rook","arithmetic","paint","library","ketchup","candle"],
-  masteredWords:["balloon"],
-
+  words:[
+    {id:0, word:"bird", audio:"/bird.mp3"},
+    {id:1, word:"baseball", audio:"/baseball.mp3"},
+    {id:2, word:"ball", audio:"/ball.mp3"},
+    {id:3, word:"therefore", audio:"/therefore.mp3"},
+    {id:4, word:"rook", audio:"/rook.mp3"},
+    {id:5, word:"arithmetic", audio:"/arithmetic.mp3"},
+    {id:6, word:"paint", audio:"/paint.mp3"},
+    {id:7, word:"library", audio:"/library.mp3"},
+    {id:8, word:"ketchup", audio:"/ketchup.mp3"},
+    {id:9, word:"candle", audio:"/candle.mp3"},
+    {id:10, word:"eighty", audio:"/eighty.mp3"},
+    {id:11, word:"litter", audio:"/litter.mp3"},
+    {id:12, word:"mayflower", audio:"/mayflower.mp3"},
+    {id:13, word:"average", audio:"/average.mp3"},
+    {id:14, word:"discontinue", audio:"/discontinue.mp3"},
+    {id:15, word:"perform", audio:"/perform.mp3"},
+    {id:16, word:"compare", audio:"/compare.mp3"},
+    {id:17, word:"calendar", audio:"/calendar.mp3"},
+    {id:18, word:"preach", audio:"/preach.mp3"},
+    {id:19, word:"declare", audio:"/declare.mp3"},
+    {id:20, word:"relief", audio:"/relief.mp3"},
+    {id:21, word:"senate", audio:"/senate.mp3"},
+    {id:22, word:"display", audio:"/display.mp3"},
+    {id:23, word:"moisture", audio:"/moisture.mp3"},
+    {id:24, word:"create", audio:"/create.mp3"},
+    {id:25, word:"poem", audio:"/poem.mp3"},
+    {id:26, word:"dwell", audio:"/dwell.mp3"},
+    {id:27, word:"hinge", audio:"/hinge.mp3"},
+    {id:28, word:"intense", audio:"/intense.mp3"},
+    {id:29, word:"meter", audio:"/meter.mp3"},
+    {id:30, word:"command", audio:"/command.mp3"},
+    {id:31, word:"umpire", audio:"/umpire.mp3"},
+    {id:32, word:"twilight", audio:"/twilight.mp3"},
+    {id:33, word:"rude", audio:"/rude.mp3"},
+    {id:34, word:"tiniest", audio:"/tiniest.mp3"},
+    {id:35, word:"criminal", audio:"/criminal.mp3"},
+    {id:36, word:"jewel", audio:"/jewel.mp3"},
+    {id:37, word:"mood", audio:"/mood.mp3"},
+    {id:38, word:"growth", audio:"/growth.mp3"},
+    {id:39, word:"population", audio:"/population.mp3"}
+  ],
+  myWords:[
+    {id:0, word:"bird", audio:"/bird.mp3"},
+    {id:1, word:"baseball", audio:"/baseball.mp3"},
+    {id:2, word:"ball", audio:"/ball.mp3"},
+    {id:3, word:"therefore", audio:"/therefore.mp3"},
+    {id:4, word:"rook", audio:"/rook.mp3"},
+    {id:5, word:"arithmetic", audio:"/arithmetic.mp3"},
+    {id:6, word:"paint", audio:"/paint.mp3"},
+    {id:7, word:"library", audio:"/library.mp3"},
+    {id:8, word:"ketchup", audio:"/ketchup.mp3"},
+    {id:9, word:"candle", audio:"/candle.mp3"}
+  ],
+  masteredWords:[
+    {id:39, word:"population", audio:"/population.mp3"}
+  ],
 }
 
 export const wordSlice = createSlice({
   name: 'wordSlice',
   initialState,
   reducers: {
+    //action.payload should be in the form {id:39, word:"population", audio:"/population.mp3"}
     addMyWords:(state, action) => {
         state.myWords.push(action.payload);
     },
     removeMyWords:(state,action)=>{
-        const newArray = state.myWords.filter((word)=> word!==action.payload);
+        const newArray = state.myWords.filter((id) => id!==action.payload.id);
         state.myWords = newArray;
     },
     addMasteredWords:(state, action) => {
         state.masteredWords.push(action.payload);
     },
     removeMasteredWords:(state,action)=>{
-        const newArray = state.masteredWords.filter((word)=> word!==action.payload );
+        const newArray = state.masteredWords.filter((id) => id!==action.payload.id );
         state.masteredWords = newArray;
     },
 
 
   },
 })
-//if filter() doesn't work
-//const newArr = array.filter()
-//state.array = newArr
-
 // Action creators are generated for each case reducer function
 export const { addMyWords, removeMyWords, addMasteredWords, removeMasteredWords } = wordSlice.actions
-
 export default wordSlice.reducer
-
